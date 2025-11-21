698e9067417a669c8d8bb5a302e9e78697561737
jeongdeoksu <jeongdeoksu@gmail.com>
2025-11-21 21:31:15 +0900

    Home 페이지 구현하기 1. UI

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import DairyList from '../components/DiaryList';
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Header
+        title="2025년 11월"
+        leftChild={<Button text={'<'} />}
+        rightChild={<Button text={'>'} />}
+      />
+      <DairyList />
     </div>
   );
 };
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,11 +3,9 @@ import './Button.css';
 
 const Button = ({ text, type, onClick }) => {
   return (
-    <div>
-      <button onClick={onClick} className={`Button Button_${type}`}>
-        {text}
-      </button>
-    </div>
+    <button onClick={onClick} className={`Button Button_${type}`}>
+      {text}
+    </button>
   );
 };
 
